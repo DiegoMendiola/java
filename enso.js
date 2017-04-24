203556fd146514544a9ed8883b98cab03eb71ffc
DiegoMendiola <diego0303@live.com.mx>
2017-04-24 14:38:18 -0500

    Add files via upload

--- enso.js
+++ enso.js
@@ -1,35 +1,63 @@
 
+
 console.log("hola");
 
-d3.select("#raton").style("border", "5px darkgray dashed");
+d3.select("#lolo").style("border", "5px darkgray dashed");
 
 d3.select("body").append("div")
 .style("border", "1px black solid")
-.html("hola mundo");
+.html("Gráfica");
+
+var prub = new Array;
+var prub2 = new Array;
+
+var prueb = [];
+
+var lay = [];
 
 
-d3.json("https://www.ncdc.noaa.gov/teleconnections/enso/indicators/soi/data.json",function(gato) {
-    console.log(gato);
-    var perro=[];
-    for(i=0;i<7;i++){
-        perro.push(i);
-    }
-    d3.select("svg")
+
+
+
+
+d3.json("http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=diego03",function(data) {
+    var valores=[];
+    
+
+    for(var i=0;i<10;i++){
+
+        console.log(data.geonames[i].lng);
+        lay [i] = data.geonames[i].lng;
+       
+        //lay[i].push;
+
+        if(lay[i]<0){
+
+            lay[i]=lay[i]*(-1)
+
+        }
+
+d3.select("svg")
 .selectAll("rect")
-.data(perro)
+.data(lay)
 .enter()
 .append("rect")
-.attr("width", 57)
+
+
+.attr("width", 30)
 .attr("height", function(d) {return d})
-.style("fill", "blue")
-.style("stroke", "red")
-.style("stroke-width", "1px")
-.style("opacity", .25)
+.style("fill", "#00FFE2")
+.style("stroke", "#2F00FF")
+.style("stroke-width", "3px")
+.style("opacity", .40)
 .attr("x", function(d,i) {return i * 57;})
 .attr("y", function(d) {return 380 - d;});
-    
-    });
+    }    
+});
 
+//prueb.push = parseFloat(lay.push);
+//prueb.push = (lay*100);
+//console.log(lay);
 
 //Funciones basicas
 parseInt("77");
